35c7ab0fe6ecb8f5f3fd635e4b199e8958bb8368
Andreas Fleck <fleckiiiiiiiimit8i+github@gmail.com>
2023-08-04 17:10:13 +0200

    make styling optional

--- main.py
+++ main.py
@@ -13,6 +13,35 @@ import io
 import base64
 import pathlib
 
+IMG_PATH = pathlib.Path(__file__).parent / "pvbatco_1.png"
+
+
+def get_qrcode_bytes(text_b64, with_image: bool = False, styled: bool = False) -> bytes:
+    text = base64.b64decode(text_b64).decode("utf-8")
+
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+
+    img = qr.make_image(
+        image_factory=StyledPilImage if styled else None,
+        module_drawer=GappedSquareModuleDrawer(),
+        color_mask=RadialGradiantColorMask(
+            center_color=(255, 0, 0), edge_color=(0, 0, 255)
+        ),
+        embeded_image_path=IMG_PATH if with_image else None,
+    )
+    f = io.BytesIO()
+    img.save(f, "PNG")
+
+    return f.getvalue()
+
+
 app = FastAPI()
 
 
@@ -28,34 +57,10 @@ async def root():
     response_class=FileResponse,
     response_description="Return a qrcode as png-file from a base64 encoded text",
 )
-async def get_qrcode(text_b64: str = "SGVsbG8gV29ybGQ="):
-    IMG_PATH = pathlib.Path(__file__).parent / "pvbatco_1.png"
-
-    text = base64.b64decode(text_b64).decode("utf-8")
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_H,
-        box_size=10,
-        border=4,
+async def get_qrcode(
+    text_b64: str = "SGVsbG8gV29ybGQ=", with_image: bool = False, styled: bool = False
+) -> Response:
+    return Response(
+        get_qrcode_bytes(text_b64, with_image=with_image, styled=styled),
+        media_type="image/png",
     )
-    qr.add_data(text)
-    qr.make(fit=True)
-
-    img = qr.make_image(
-        # fill_color="black",
-        # back_color="white",
-        image_factory=StyledPilImage,
-        module_drawer=GappedSquareModuleDrawer(),
-        color_mask=RadialGradiantColorMask(
-            center_color=(255, 0, 0), edge_color=(0, 0, 255)
-        ),
-        embeded_image_path=IMG_PATH,
-    )
-    # img = qrcode.make(text)
-    f = io.BytesIO()
-    img.save(f, "PNG")
-
-    # with open("qrcode.png", "wb") as f2:
-    #     img.save(f2, "PNG")
-    # img.save("qrcode_gapped.png")
-    return Response(f.getvalue(), media_type="image/png")
